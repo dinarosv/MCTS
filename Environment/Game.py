5f5d4a0aa4ed5098518bcdef0f047d90ed04ab23
Dina Rosvoll <dinaro@stud.ntnu.no>
2020-03-02 15:38:30 +0100

    NIM game

--- Environment/Game.py
+++ Environment/Game.py
@@ -1,16 +1,16 @@
 
 class Game:
-  def __init__(self):
-    self.state = None
+    def __init__(self):
+        self.state = None
 
-  def get_state(self):
-    return self.state
+    def get_state(self):
+        return self.state
 
-  def get_actions(self):
-    raise NotImplementedError
+    def get_actions(self):
+        raise NotImplementedError
 
-  def do_action(self, action):
-    raise NotImplementedError
+    def do_action(self, action):
+        raise NotImplementedError
 
-  def is_final_state(self):
-    raise NotImplementedError
+    def is_final_state(self):
+        raise NotImplementedError
--- Environment/NIM.py
+++ Environment/NIM.py
@@ -1,5 +1,20 @@
 from Environment.Game import *
 
 class NIM(Game):
-  def print_board(self):
-    print("booooard")
+  def __init__(self, N, K): #N:number of pieces, K: max pieces to remove
+    self.N = N
+    self.K = K
+
+  def get_state(self):
+    return self.N
+
+  def get_actions(self):
+    return [x+1 for x in range(self.K) if x+1 <= self.N]
+
+  def do_action(self, action):
+    self.N -= action
+    if self.N < 0:
+      self.N = 0
+
+  def is_final_state(self):
+    return self.N == 0 
