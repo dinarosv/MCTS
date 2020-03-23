2e2546c0bb2e923b54412cf1e853bf11628cd0e8
Unknown <cbrustand@hotmail.com>
2020-03-23 13:42:51 +0100

    Added comments

--- MCTS.py
+++ MCTS.py
@@ -10,6 +10,7 @@ class MCTS(Agent):
 
     # Return action that leads to best state after having performed simulations
     def get_action(self, player):
+        #Initialize a new root node with the current game state before the simulations start
         self.root_node = MCTSNode(self.state_manager, init_player=player)
         for _ in range(self.simulations): #Number of simulations
 
@@ -19,45 +20,51 @@ class MCTS(Agent):
             # Expansion
             node.expand_child_nodes()
 
+            #Find a child to do rollouts on unless it is a terminal node
             if node.is_terminal_node():
                 leaf = node
             else:
                 leaf = node.get_child(self.behavior_policy(node))
 
             # Leaf evaluation
-            winner = self.perform_rollout(leaf)
+            winner = self.evaluate_leaf_node(leaf)
 
             #backpropagation
             self.backpropagate(leaf, winner)
 
         # Choose action
-        action = self.root_node.get_most_visited_action()
-        return action
+        return self.root_node.get_most_visited_action()
 
     def selection(self):
         node = self.root_node
+        #Keep propagating down towards a leaf node from the root
         while not node.is_leaf_node():
-            # Get action following tree policy (choosing highest/lowest value action)
+            # Get action following tree policy
             action = node.get_best_action()
             node = node.get_child(action)
         return node
 
-    def perform_rollout(self, node):
+    #Gets the winner of a rollout
+    def evaluate_leaf_node(self, node):
         result = self.rollout(node)
         node.prune_children()
         return result
 
+    #Recursive function to get to a terminal node to find a victor
     def rollout(self, node):
         if node.is_terminal_node():
             #The player who made the move to get to this state is the winner
             return abs(node.player_to_move - 1)
         node.expand_child_nodes()
+        #Use behavior policy in rollout. For us it is a random choice from children
         action = self.behavior_policy(node)
         node = node.get_child(action)
         return self.rollout(node)
 
     def backpropagate(self, node, value):
+        #Feed the result to the backpropagate-function of the node that the rollout was performed on
         node.backpropagate(value)
 
     def behavior_policy(self, node):
+        #Our behavior policy is random
         return random.sample(list(node.children), 1)[0]
--- Node.py
+++ Node.py
@@ -1,5 +1,6 @@
 import math
 
+#A class representing a node-structure not concerning any evaluation of nodes.
 class Node:
     def __init__(self, state_manager, state=None, parent=None):
         self.state_manager = state_manager
@@ -19,42 +20,47 @@ class Node:
     def prune_children(self):
         self.children = dict()
 
-
+#A class inheriting the general node structure above, but adding the complexity of MCTS-logic
 class MCTSNode(Node):
     def __init__(self, state_manager, state=None, parent=None, init_player=None):
         super().__init__(state_manager, state, parent)
-        #Fix so that this gets set correctly initially
-        if not init_player == None:
-            self.player_to_move = init_player
-        else:
+        if init_player == None:
             self.player_to_move = abs(self.parent.player_to_move - 1)
+        else:
+            self.player_to_move = init_player
+            
         self.visits = 0
         self.results = {0: 0, 1: 0}
 
+    #Returns the win rate of a given player in the state of the node
     def win_rate(self, player):
         if self.visits == 0:
             return math.inf
         return self.results[player] / (self.visits)
 
-    def get_best_action(self, c=1.4):
+    #This is our tree policy, and is based on win ratio and an exploration bonus.
+    def get_best_action(self, c=1):
         values = dict()
         for action, child in self.children.items():
             values[action] = math.inf if child.visits == 0 else child.win_rate(self.player_to_move) + c * math.sqrt(math.log(self.visits) / child.visits)
         return max(values, key=values.get)
 
+    #To pick the action to actually do in a state, we pick the action that was picked the most throughout the simulations
     def get_most_visited_action(self):
         values = dict()
         for action, child in self.children.items():
             values[action] = child.visits
         return max(values, key=values.get)
 
-
+    #Backpropagate recursively the winning player up through the node-hierarchy
     def backpropagate(self, winning_player):
+        #We update the statistics to ensure the correct win rate
         self.visits += 1
         self.results[winning_player] += 1
         if self.parent:
             self.parent.backpropagate(winning_player)
 
+    #Calls the state manager to get possible child states from a given state, and initializes child nodes for these states.
     def expand_child_nodes(self):
         for action, child_state in self.state_manager.generate_child_states(self.state):
             self.children[action] = MCTSNode(self.state_manager, child_state, self)
